37b721402a1ce21ec2d888a29bcb8e45a4510a8b
Glenn Ricaud <gri@enonic.com>
2017-10-10 14:58:19 +0200

    Starter Workbox - Generated resources should not be in src folder (#3)

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const extractTextPlugin = require('extract-text-webpack-plugin');
-const workboxBuild = require('workbox-build');
+const workboxPlugin = require('workbox-webpack-plugin');
 
 const paths = {
     assets: 'src/main/resources/assets/',
@@ -9,14 +9,6 @@ const paths = {
 
 const assetsPath = path.join(__dirname, paths.assets);
 const buildAssetsPath = path.join(__dirname, paths.buildAssets);
-
-workboxBuild.injectManifest({
-    swSrc: path.join(assetsPath, 'js/sw-dev.js'),
-    swDest: path.join(buildAssetsPath, 'sw-template.js'),
-    globDirectory: assetsPath,
-    globPatterns: ['precache/**\/*'],
-    globIgnores: ['precache/browserconfig.xml', 'precache/manifest.json']
-});
 
 module.exports = {
 
@@ -43,7 +35,14 @@ module.exports = {
         ]
     },
     plugins: [
-        new extractTextPlugin('precache/bundle.css')
+        new extractTextPlugin('precache/bundle.css'),
+        new workboxPlugin({
+            globDirectory: buildAssetsPath,
+            globPatterns: ['precache/**\/*'],
+            globIgnores: [],
+            swSrc: path.join(assetsPath, 'js/sw-dev.js'),
+            swDest: path.join(buildAssetsPath, 'sw-template.js')
+        })
     ]
 
 };
