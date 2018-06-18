344c3ce092752cb27b8360cca31041a200160dc4
olaven <olavsundfoer@gmail.com>
2018-06-18 15:18:44 +0200

    user gets a notice if no text is present #18

--- src/main/resources/assets/js/background-sync.js
+++ src/main/resources/assets/js/background-sync.js
@@ -1,18 +1,32 @@
 // Listeners 
 document.getElementById("add-todo-button").onclick = (event) => {
     const now = new Date(); 
-    const text = document.getElementById("add-todo-text").value; 
+    const inputfield = document.getElementById("add-todo-text"); 
     const date = now.getDay() + "/" + (now.getMonth() + 1) + "/" + now.getFullYear(); 
 
     let outputArea = document.getElementById("todo-app__item-area"); 
 
-    outputArea.innerHTML += `
-        <div class="todo-app__item">
-            <input type="checkbox">
-            <div>
-                <div>${text}</div>
-                <div>${date}</div>
+    // Only add if user actually entered something 
+    if(inputfield.value !== "") {
+        outputArea.innerHTML += `
+            <div class="todo-app__item">
+                <input type="checkbox">
+                <div>
+                    <div>${inputfield.value}</div>
+                    <div>${date}</div>
+                </div>
             </div>
-        </div>
-    `;
+        `;
+
+        //empty the field 
+        inputfield.value = ""; 
+    } else {
+        // let user know something was wrong 
+        inputfield.style.border = "solid red"; 
+        setTimeout(() => {
+            inputfield.style.border = "";
+        }, 500); 
+    }
+
+    
 }
