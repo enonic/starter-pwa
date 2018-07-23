2934bd769a87fee8463dc2a8e92b31b0562ba971
olaven <olavsundfoer@gmail.com>
2018-07-23 13:38:53 +0200

    linter approves app.js #23

--- src/main/resources/assets/js/app.js
+++ src/main/resources/assets/js/app.js
@@ -1,43 +1,44 @@
 // Stylesheets
 require('../css/styles.less');
-require("../css/background-sync.less"); //having this in background-sync.js overwrites the other styles
+require('../css/background-sync.less'); // having this in background-sync.js overwrites the other styles
 
-const storageManager = require('./libs/background-sync/storage-manager'); 
+const storageManager = require('./libs/background-sync/storage-manager');
 
-var ToasterInstance = require("./libs/Toaster").default;
+var ToasterInstance = require('./libs/Toaster').default;
 
 module.exports = {
     notifyAboutNewVersion: function() {
         var snackbarContainer = document.querySelector('#notification-bar');
-        var data = {message: 'PWA Starter upgraded to the latest version'};
+        var data = { message: 'PWA Starter upgraded to the latest version' };
         if (snackbarContainer.MaterialSnackbar) {
             snackbarContainer.MaterialSnackbar.showSnackbar(data);
         }
-    },
+    }
 };
 
-(function(){
+(function() {
     window.onload = function() {
-        const mainContainer = document.getElementById("main-container");
+        const mainContainer = document.getElementById('main-container');
 
         if (!mainContainer) {
             return;
         }
 
-        const toggleOnlineStatus = function () {
+        const toggleOnlineStatus = function() {
             if (navigator.onLine) {
-                storageManager("online")
-            }else{
+                storageManager('online');
+            } else {
                 ToasterInstance().then(toaster => {
                     toaster.toast('Connection is off.');
-            })};
-            mainContainer.classList.toggle("online", navigator.onLine);
-            mainContainer.classList.toggle("offline", !navigator.onLine);
+                });
+            }
+            mainContainer.classList.toggle('online', navigator.onLine);
+            mainContainer.classList.toggle('offline', !navigator.onLine);
         };
 
         toggleOnlineStatus();
 
-        window.addEventListener("offline", toggleOnlineStatus);
-        window.addEventListener("online", toggleOnlineStatus);
+        window.addEventListener('offline', toggleOnlineStatus);
+        window.addEventListener('online', toggleOnlineStatus);
     };
 })();
