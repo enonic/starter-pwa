b792f67c4cf4346e4832d3f09d3120456ba1c87c
olaven <olavsundfoer@gmail.com>
2018-07-23 14:45:33 +0200

    linter approves of indexed-db.js #23

--- src/main/resources/assets/js/libs/background-sync/Storage.js
+++ src/main/resources/assets/js/libs/background-sync/Storage.js
@@ -1,124 +1,127 @@
-const IndexedDBInstance = require('./db/indexed-db').default; 
-const storageManager = require('./storage-manager'); 
+const IndexedDBInstance = require('./db/indexed-db').default;
+const storageManager = require('./storage-manager');
 
 export default {
     /**
      * Methods for retrieving from storage
      */
-    get : {
+    get: {
         /**
          * Gets items from indexDB
-         * @param storeName name of the indexDB store 
-         * @param callback callback when fetched 
+         * @param storeName name of the indexDB store
+         * @param callback callback when fetched
          */
         offline: (storeName, callback) => {
-            return IndexedDBInstance().then(instance => 
+            return IndexedDBInstance().then(instance =>
                 instance.getAll(storeName).then(callback)
             );
-        }, 
+        },
         /**
-         * Get request from URL 
-         * @param url the url to fetch from 
-         * @returns Promise from fetch 
+         * Get request from URL
+         * @param url the url to fetch from
+         * @returns Promise from fetch
          */
         online: (url, data) => {
-            return fetch(url + "?data=" + String(data),{
+            return fetch(url + '?data=' + String(data), {
                 method: 'GET'
-            })
+            });
         }
-    }, 
+    },
     /**
      * Methods for adding to storage
      */
-    add : { 
+    add: {
         /**
          * Adds item to indexDB
-         * @param storeName name of the indexDB store 
-         * @param item the item to add 
-         * @param noSync set to true if no sync with repo is wanted 
+         * @param storeName name of the indexDB store
+         * @param item the item to add
+         * @param noSync set to true if no sync with repo is wanted
          */
-        offline : (storeName, item, sync) => {
+        offline: (storeName, item, sync) => {
             return IndexedDBInstance().then(instance => {
-                instance.add(storeName, item);   
-                if(!sync){
-                    storageManager("delete"); 
-                } 
-            }); 
-        }, 
+                instance.add(storeName, item);
+                if (!sync) {
+                    storageManager('delete');
+                }
+            });
+        },
         /**
-         * Adds item to online storage 
-         * @param url the url to add to 
-         * @param data to be posted 
+         * Adds item to online storage
+         * @param url the url to add to
+         * @param data to be posted
          */
-        online : (url, data) => {
+        online: (url, data) => {
             return fetch(url, {
                 body: JSON.stringify(data),
-                method: 'POST',
-            })
+                method: 'POST'
+            });
         }
-    }, 
+    },
     /**
      * Methods for deleting from storage
      */
-    delete : {
+    delete: {
         /**
          * Removes item from indexDB
          * @param storeName name of the indexDB store
-         * @param identifier the identifier of item to delete 
+         * @param identifier the identifier of item to delete
          * @param sync If function called by online sync function
          */
-        offline : (storeName, identifier, sync) => {
-            return IndexedDBInstance().then(instance => {
-                let req = instance.delete(storeName, identifier)
-                //req.onsuccess = () => storageManager("delete");  
-                if(!sync){
-                    storageManager("delete"); 
+        offline: (storeName, identifier, sync) => {
+            // keeping this for now, as I know it caused an error that I can check once the linter lets me build
+            // return IndexedDBInstance().then(instance => {
+            //     let req = instance.delete(storeName, identifier);
+            //     // req.onsuccess = () => storageManager("delete");
+            //     if (!sync) {
+            //         storageManager('delete');
+            //     }
+            // });
+            return IndexedDBInstance().then(() => {
+                if (!sync) {
+                    storageManager('delete');
                 }
             });
-            
         },
         /**
          * Removes item from online storage
-         * @param url the url to call DELETE method on 
+         * @param url the url to call DELETE method on
          * @param parameter passed as ?data to specify what to delete
-         * @returns Promise from fetch 
+         * @returns Promise from fetch
          */
-        online : (url, data) => {
-            return fetch(url + "?data=" + JSON.stringify(data), {
-                method: 'DELETE',
-            })
+        online: (url, data) => {
+            return fetch(url + '?data=' + JSON.stringify(data), {
+                method: 'DELETE'
+            });
         }
-    }, 
+    },
     /**
      * Methods for replacing things in storage
      */
-    replace : {
+    replace: {
         /**
          * Edits item in indexDB store
-         * @param storeName name of the indexDB store 
-         * @param item item to replace the old one 
+         * @param storeName name of the indexDB store
+         * @param item item to replace the old one
          */
-        offline : (storeName, item) => {
+        offline: (storeName, item) => {
             return IndexedDBInstance().then(instance => {
                 instance.put(storeName, item);
-                storageManager("replace"); 
+                storageManager('replace');
             });
-            
-        }, 
-        online : (url, data) => {
+        },
+        online: (url, data) => {
             return fetch(url, {
-                body: JSON.stringify(data), 
-                method: 'PUT',
-            }) 
+                body: JSON.stringify(data),
+                method: 'PUT'
+            });
         }
     },
 
-    flush : {
-        offline : (storeName) => {
+    flush: {
+        offline: storeName => {
             return IndexedDBInstance().then(instance => {
-                instance.deleteAll(storeName)
-    
-            })
+                instance.deleteAll(storeName);
+            });
         }
     }
-}
+};
--- src/main/resources/assets/js/libs/background-sync/db/indexed-db.js
+++ src/main/resources/assets/js/libs/background-sync/db/indexed-db.js
@@ -17,68 +17,47 @@
 
 import ConfigManagerInstance from './Config';
 
-export default function IndexedDBInstance() {
-
-    if (typeof window.IndexedDBInstance_ !== 'undefined')
-        return Promise.resolve(window.IndexedDBInstance_);
-
-    window.IndexedDBInstance_ = new OfflineDatabase();
-
-    return Promise.resolve(window.IndexedDBInstance_);
-}
-
 class OfflineDatabase {
-
     constructor() {
-
-        ConfigManagerInstance().then((configManager) => {
-
+        ConfigManagerInstance().then(configManager => {
             var config = configManager.config;
 
             this.db_ = null;
             this.name_ = config.name;
             this.version_ = config.version;
             this.stores_ = config.stores;
-
         });
     }
 
     getStore(storeName) {
-
         if (!this.stores_[storeName])
-            throw 'There is no store with name "' + storeName + '"';
+            throw new Error('There is no store with name "' + storeName + '"');
 
         return this.stores_[storeName];
     }
 
     open() {
-        if (this.db_)
-            return Promise.resolve(this.db_);
+        if (this.db_) return Promise.resolve(this.db_);
 
         return new Promise((resolve, reject) => {
-
             var dbOpen = indexedDB.open(this.name_, this.version_);
 
-            dbOpen.onupgradeneeded = (e) => {
-
+            dbOpen.onupgradeneeded = e => {
                 this.db_ = e.target.result;
 
                 var storeNames = Object.keys(this.stores_);
                 var storeName;
 
                 for (var s = 0; s < storeNames.length; s++) {
-
                     storeName = storeNames[s];
 
                     // If the store already exists
                     if (this.db_.objectStoreNames.contains(storeName)) {
-
                         // Check to see if the store should be deleted.
                         // If so delete, and if not skip to the next store.
                         if (this.stores_[storeName].deleteOnUpgrade)
                             this.db_.deleteObjectStore(storeName);
-                        else
-                            continue;
+                        // else continue;
                     }
 
                     var dbStore = this.db_.createObjectStore(
@@ -86,7 +65,9 @@ class OfflineDatabase {
                         this.stores_[storeName].properties
                     );
 
-                    if (typeof this.stores_[storeName].indexes !== 'undefined') {
+                    if (
+                        typeof this.stores_[storeName].indexes !== 'undefined'
+                    ) {
                         var indexes = this.stores_[storeName].indexes;
                         var indexNames = Object.keys(indexes);
                         var index;
@@ -97,148 +78,130 @@ class OfflineDatabase {
                         }
                     }
                 }
-            }
-
-            dbOpen.onsuccess = (e) => {
-                this.db_ = e.target.result;
-                resolve(this.db_);
-            }
-
-            dbOpen.onerror = (e) => {
-                reject(e);
             };
 
+            dbOpen.onsuccess = e => {
+                this.db_ = e.target.result;
+                resolve(this.db_);
+            };
+
+            dbOpen.onerror = e => {
+                reject(e);
+            };
         });
     }
 
     close() {
-
         return new Promise((resolve, reject) => {
-
-            if (!this.db_)
-                reject('No database connection');
+            if (!this.db_) reject(new Error('No database connection'));
 
             this.db_.close();
             resolve(this.db_);
-
         });
     }
 
     nuke() {
         return new Promise((resolve, reject) => {
-
-            console.log("Nuking... " + this.name_);
+            // console.log('Nuking... ' + this.name_);
 
             this.close();
 
             var dbTransaction = indexedDB.deleteDatabase(this.name_);
-            dbTransaction.onsuccess = (e) => {
-                console.log("Nuked...");
+            dbTransaction.onsuccess = e => {
+                // console.log('Nuked...');
                 resolve(e);
-            }
+            };
 
-            dbTransaction.onerror = (e) => {
+            dbTransaction.onerror = e => {
                 reject(e);
-            }
+            };
         });
     }
 
     put(storeName, value) {
-
-        return this.open().then((db) => {
+        return this.open().then(db => {
             return new Promise((resolve, reject) => {
-
                 var dbTransaction = db.transaction(storeName, 'readwrite');
                 var dbStore = dbTransaction.objectStore(storeName);
                 var dbRequest = dbStore.put(value);
 
-                dbTransaction.oncomplete = (e) => {
+                dbTransaction.oncomplete = () => {
                     resolve(dbRequest.result);
-                }
+                };
 
-                dbTransaction.onabort =
-                    dbTransaction.onerror = (e) => {
-                        reject(e);
-                    }
+                // dbTransaction.onabort = dbTransaction.onerror = e => {
+                //     reject(e);
+                // };
 
+                dbTransaction.onabort = e => reject(e);
+                dbTransaction.onerror = e => reject(e);
             });
-
         });
-
-    } 
+    }
 
     add(storeName, value) {
-        
-        return this.open().then((db) => {
-
+        return this.open().then(db => {
             return new Promise((resolve, reject) => {
-
                 var dbTransaction = db.transaction(storeName, 'readwrite');
                 var dbStore = dbTransaction.objectStore(storeName);
-                var dbRequest = dbStore.add(value/*, key*/);
+                var dbRequest = dbStore.add(value /* , key */);
 
-                dbTransaction.oncomplete = (e) => {
+                dbTransaction.oncomplete = () => {
                     resolve(dbRequest.result);
-                }
+                };
 
-                dbTransaction.onabort =
-                    dbTransaction.onerror = (e) => {
-                        reject(e);
-                    }
+                // dbTransaction.onabort = dbTransaction.onerror = e => {
+                //     reject(e);
+                // };
 
+                dbTransaction.onabort = e => reject(e);
+                dbTransaction.onerror = e => reject(e);
             });
-
         });
-
     }
 
     get(storeName, value) {
-
-        return this.open().then((db) => {
-
+        return this.open().then(db => {
             return new Promise((resolve, reject) => {
-
                 var dbTransaction = db.transaction(storeName, 'readonly');
                 var dbStore = dbTransaction.objectStore(storeName);
                 var dbStoreRequest;
 
-                dbTransaction.oncomplete = (e) => {
+                dbTransaction.oncomplete = () => {
                     resolve(dbStoreRequest.result);
-                }
+                };
 
-                dbTransaction.onabort =
-                    dbTransaction.onerror = (e) => {
-                        reject(e);
-                    }
+                // dbTransaction.onabort = dbTransaction.onerror = e => {
+                //     reject(e);
+                // };
+
+                dbTransaction.onabort = e => reject(e);
+                dbTransaction.onerror = e => reject(e);
 
                 dbStoreRequest = dbStore.get(value);
-
             });
-
         });
-
     }
 
     getAll(storeName, index, order) {
-        return this.open().then((db) => {
-
+        return this.open().then(db => {
             return new Promise((resolve, reject) => {
-
                 var dbTransaction = db.transaction(storeName, 'readonly');
                 var dbStore = dbTransaction.objectStore(storeName);
                 var dbCursor;
 
-                if (typeof order !== 'string')
-                    order = 'next';
+                let changedOrder = order;
+                if (typeof order !== 'string') changedOrder = 'next';
 
                 if (typeof index === 'string')
-                    dbCursor = dbStore.index(index).openCursor(null, order);
-                else
-                    dbCursor = dbStore.openCursor();
+                    dbCursor = dbStore
+                        .index(index)
+                        .openCursor(null, changedOrder);
+                else dbCursor = dbStore.openCursor();
 
                 var dbResults = [];
 
-                dbCursor.onsuccess = (e) => {
+                dbCursor.onsuccess = e => {
                     var cursor = e.target.result;
 
                     if (cursor) {
@@ -250,67 +213,69 @@ class OfflineDatabase {
                     } else {
                         resolve(dbResults);
                     }
-                }
+                };
 
-                dbCursor.onerror = (e) => {
+                dbCursor.onerror = e => {
                     reject(e);
-                }
-
+                };
             });
-
         });
     }
 
     delete(storeName, key) {
-        return this.open().then((db) => {
-
+        return this.open().then(db => {
             return new Promise((resolve, reject) => {
                 var dbTransaction = db.transaction(storeName, 'readwrite');
                 var dbStore = dbTransaction.objectStore(storeName);
 
-                dbTransaction.oncomplete = (e) => {
+                dbTransaction.oncomplete = e => {
                     resolve(e);
-                }
+                };
 
-                dbTransaction.onabort =
-                    dbTransaction.onerror = (e) => {
-                        reject(e);
-                    }
-                if (this.get(storeName, key)){
+                // dbTransaction.onabort = dbTransaction.onerror = e => {
+                //     reject(e);
+                // };
+
+                dbTransaction.onabort = e => reject(e);
+                dbTransaction.onerror = e => reject(e);
+
+                if (this.get(storeName, key)) {
                     dbStore.delete(key);
                 } else {
-                    reject({
-                        status : 404,
-                        message : "NOT FOUND"
-                    })
+                    // reject({
+                    //     status: 404,
+                    //     message: 'NOT FOUND'
+                    // });
+                    reject(new Error('NOT FOUND'));
                 }
-                
-
             });
         });
     }
 
     deleteAll(storeName) {
-
-        return this.open().then((db) => {
-
+        return this.open().then(db => {
             return new Promise((resolve, reject) => {
-
                 var dbTransaction = db.transaction(storeName, 'readwrite');
                 var dbStore = dbTransaction.objectStore(storeName);
                 var dbRequest = dbStore.clear();
 
-                dbRequest.onsuccess = (e) => {
+                dbRequest.onsuccess = e => {
                     resolve(e);
-                }
+                };
 
-                dbRequest.onerror = (e) => {
+                dbRequest.onerror = e => {
                     reject(e);
-                }
-
+                };
             });
-
         });
     }
-    
+}
+
+export default function IndexedDBInstance() {
+    if (typeof window.IndexedDBInstance_ !== 'undefined')
+        return Promise.resolve(window.IndexedDBInstance_);
+
+    window.IndexedDBInstance_ = new OfflineDatabase();
+
+    return Promise.resolve(window.IndexedDBInstance_);
 }
