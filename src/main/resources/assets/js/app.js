3a3974f6ea61c4efd6ae715b243eea340fc3b77a
alansemenov <ase@enonic.com>
2023-08-10 17:07:02 +0200

    No notification given after Service Worker update #437

--- src/main/resources/assets/js/app.js
+++ src/main/resources/assets/js/app.js
@@ -1,36 +1,75 @@
 import ToasterInstance from './toaster';
+import '../css/app.less';
+import toast from 'toast-me';
 
 const showNotification = (registration, appName) => {
-    const snackbarContainer = document.querySelector('#notification-bar');
-    const handler = function () {
+    const handler = (myToast) => {
+        myToast.close();
         if (!registration.waiting) {
             // Just to ensure registration.waiting is available before
             // calling postMessage()
             document.location.reload();
             return;
         }
-
-        registration.waiting.postMessage('skipWaiting');
-        document.location.reload();
+        if ('Notification' in window) {
+            Notification.requestPermission().then((permission) => {
+                if (permission === 'granted') {
+                    // Permission has been granted, show notifications.
+                    registration.waiting.postMessage('skipWaitingAndNotify');
+                } else {
+                    // Permission has not been granted. Notifications will not work.
+                    registration.waiting.postMessage('skipWaiting');
+                }
+            });
+        }
     };
 
-    if (snackbarContainer.MaterialSnackbar) {
-        const data = {
-            message: `New version of ${appName} is available`,
-            actionHandler: handler,
-            actionText: 'Update',
-            timeout: 100000
-        };
-        snackbarContainer.MaterialSnackbar.showSnackbar(data);
-    } else {
-        handler();
-    }
+    const updateNotification = toast(
+        `New version of ${appName} is available`,
+        {
+            position: 'bottom',
+            closeable: false,
+            duration: 30000,
+            toastClass: 'toast-container'
+        },
+        {
+            label: 'Update',
+            class: 'toast-button',
+            action: () => handler(updateNotification)
+        }
+    );
 };
 
 const handleNewServiceWorker = (registration, appName) => {
-    registration.addEventListener('updatefound', () =>
-        showNotification(registration, appName)
-    );
+    let updated = false;
+    let activated = false;
+
+    const checkUpdate = () => {
+        if (activated && updated) {
+            window.location.reload();
+        }
+    };
+
+    navigator.serviceWorker.addEventListener('controllerchange', () => {
+        // This will be triggered when the service worker is replaced with a new one.
+        // We do not just reload the page right away, we want to make sure we are fully activated using the checkUpdate function.
+        updated = true;
+        checkUpdate();
+    });
+
+    registration.addEventListener('updatefound', () => {
+        showNotification(registration, appName);
+
+        const worker = registration.installing;
+        worker.addEventListener('statechange', () => {
+            if (worker.state === 'activated') {
+                // Here is when the activated state was triggered from the lifecycle of the service worker.
+                // This will trigger on the first install and any updates.
+                activated = true;
+                checkUpdate();
+            }
+        });
+    });
 };
 
 (function () {
